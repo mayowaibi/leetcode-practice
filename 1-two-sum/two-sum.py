b468e869ac4af8b5c8d9a65756b787061646ed6b
Isaac Ibidun <isaac.ibidun@gmail.com>
2023-07-27 14:37:10 -0400

    Time: 68 ms (92.56%) | Memory: 17.7 MB (36.36%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapTracker = {}
-        for i in range(len(nums)):
-            if (target-nums[i]) in mapTracker:
-                return [i, mapTracker.get(target-nums[i])]
-            mapTracker[nums[i]] = i
-        return None
+        mapTracker = {} # key = num, value = index (num: index)
+        for index, num in enumerate(nums):
+            if (target-num) in mapTracker:
+                return [index, mapTracker[target-num]]
+            mapTracker[num] = index
+        return
