fadbecdb4a348239834fa18580a50d33a6d57a12
Isaac <isaac.ibidun@gmail.com>
2024-02-27 19:49:49 -0500

    Time: 108 ms (75.40%) | Memory: 17.7 MB (79.97%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,32 @@
 class Solution:
+    # Solution 1 - Difference in sums: O(1) SC, O(n) TC
     def missingNumber(self, nums: List[int]) -> int:
         result = len(nums)
-
+        
         for i in range(len(nums)):
-            result += i - nums[i]
+            result += (i - nums[i])
         return result
+
+
+    # Solution 2 - XOR: O(1) SC, O(n) TC
+    def missingNumber2(self, nums: List[int]) -> int:
+        result = 0
+
+        for i in range(0, len(nums)):
+            result ^= 1
+
+        for num in nums:
+            result ^= num
+        return result
+
+    # Solution 3 - Using a set: O(n) SC, O(n) TC
+    def missingNumber3(self, nums: List[int]) -> int:
+        hashset = set()
+
+        for i in range(len(nums) + 1):
+            hashset.add(i)
+
+        for num in nums:
+            if num in hashset:
+                hashset.remove(num)
+        return hashset.pop()
