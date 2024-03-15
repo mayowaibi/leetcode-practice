a4ad363d9b065c0f3e3d330c31ec16a2f98f3bf6
Isaac <isaac.ibidun@gmail.com>
2024-03-14 21:01:41 -0400

    Time: 46 ms (93.91%) | Memory: 16.6 MB (91.73%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -7,7 +7,7 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummyNode = ListNode()
         curr = dummyNode
-        carry = 0       # to store any value that needs to be carried to the next node
+        carry = 0   # to store any value that needs to be carried to the next node
 
         while l1 or l2 or carry:
             # v1 and v2 are the corresponding values from each linked list
@@ -15,15 +15,15 @@ class Solution:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
 
-            sum = v1 + v2 + carry
-            carry = sum // 10       # to get the carry digit if sum > 10
-            sum = sum % 10      # to ensure sum only holds the unit place value of v1+v2+carry
+            totalSum = v1 + v2 + carry
+            carry = totalSum // 10  # to get the carry digit if sum > 10
+            totalSum = totalSum % 10    # to ensure sum only holds the unit place value of totalSum
             
-            curr.next = ListNode(sum)       # add the new initialized sum node to the list
+            curr.next = ListNode(totalSum)   # add the new initialized sum node to the list
 
             # update all pointers
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         
-        return dummyNode.next       # return the entire list
+        return dummyNode.next   # return the entire list
