e7dbdadd76b02c34f942b196b1bab7917c2adb87
Isaac <isaac.ibidun@gmail.com>
2024-03-20 18:57:18 -0400

    Time: 39 ms (65.15%) | Memory: 17.5 MB (84.40%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,9 +4,46 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+class Solution: 
+    # Recursive DFS - TC: O(n), SC: O(n)
+    def maxDepth1(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-            
+        
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    # Iterative BFS - TC: O(n), SC: O(n)
+    def maxDepth2(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        # bfs using a queue, starting from the root until all nodes are visited
+        q = deque([root])
+        level = 0
+
+        while q:
+            # pop every node in the queue then add its (non-null) children
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
+
+    # Iterative DFS (Preorder) - TC: O(n), SC: O(n)
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        maxLevel = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            # for every non-null node, add it's children, whether they are null or not
+            if node:
+                maxLevel = max(maxLevel, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return maxLevel
