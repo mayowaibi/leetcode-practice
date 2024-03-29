0ccbd6229a611b9bd9f21136fb845b20a2e26829
Isaac <isaac.ibidun@gmail.com>
2024-03-29 13:30:20 -0400

    Time: 0 ms (100.00%) | Memory: 45.3 MB (91.72%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,15 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            // To prevent integer overflow with large numbers:
-            int mid = left + ((right - left) / 2); // Equivalent to (l + r) // 2
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
+        int l = 0, r = nums.length - 1;
+        
+        while (l <= r) {
+            int mid = l + ((r - l) / 2);
+            if (target < nums[mid]) {
+                r = mid - 1;
+            } else if (target > nums[mid]) {
+                l = mid + 1;
             } else {
                 return mid;
             }
