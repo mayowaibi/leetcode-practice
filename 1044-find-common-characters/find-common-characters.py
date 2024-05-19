09707edb07a137365f0405c73cf2d352acf42c04
Isaac <isaac.ibidun@gmail.com>
2024-05-19 11:06:25 -0400

    Time: 44 ms (83.65%) | Memory: 16.7 MB (72.61%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    # Hashmap (Counter) Solution - TC: O(n), SC: O(n)
+    def commonChars(self, words: List[str]) -> List[str]:
+        result = []
+
+        # Create a char counter hashmap for the first word
+        hashmap = {}
+        for c in words[0]:
+            hashmap[c] = hashmap.get(c, 0) + 1
+        
+        # For every other word in words, get the min no. of
+        # occurences for each char in the first word
+        for i in range(1, len(words)):
+            for key in hashmap:
+                hashmap[key] = min(words[i].count(key), hashmap[key])
+
+        # If the no. of occurences is 0, then the char does
+        # not appear in at least one of the words
+        for key in hashmap:
+            if hashmap[key] != 0:
+                # Loop to ensure char is added to result multiple times if needed
+                for i in range(hashmap[key]):
+                    result.append(key)
+        
+        return result
