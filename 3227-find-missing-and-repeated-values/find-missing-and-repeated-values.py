c61b0381db8f3e9fedb1407757c17fcaf0110113
Isaac <isaac.ibidun@gmail.com>
2024-07-26 23:35:42 -0400

    Time: 113 ms (79.05%) | Memory: 17 MB (42.38%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,7 +1,7 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
         numSet = set()
-        size = 0
+        numCount = 0
         res = []
 
         for i in range(len(grid)):
@@ -11,9 +11,9 @@ class Solution:
                     numSet.add(curr)
                 else:
                     res.append(curr)
-                size += 1
+                numCount += 1
         
-        for i in range(1, size + 1):
+        for i in range(1, numCount + 1):
             if i not in numSet:
                 res.append(i)
 
