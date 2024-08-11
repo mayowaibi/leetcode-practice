256feaad78e7a0e95841ba5171286869f51b2b01
Isaac <isaac.ibidun@gmail.com>
2024-08-10 21:47:05 -0400

    Time: 36 ms (48.48%) | Memory: 16.5 MB (13.09%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -4,12 +4,30 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    # Solution 1: Fast and Slow Pointer - TC: O(n), SC: O(1) 
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = head
-        fast = head
+        fast, slow = head, head
 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
+        
         return slow
+
+    # Solution 2: Double Traversal - TC: O(n), SC: O(1) 
+    def middleNode2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = head
+        count = 0
+
+        # First traversal to get size of list
+        while dummy:
+            count += 1
+            dummy = dummy.next
+        
+        mid = count // 2 + 1
+
+        # Second traversal to get middle node
+        for i in range(mid-1):
+            head = head.next
+
+        return head
