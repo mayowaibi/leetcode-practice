f948b23f8f6493d15390052fb2de2a6d58e6a649
Isaac <isaac.ibidun@gmail.com>
2024-10-07 23:29:08 -0400

    Time: 4 ms (84.74%) | Memory: 43.4 MB (49.96%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Stack Solution - TC: O(n), SC:O(n)
+    public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+            if (c == 'B' && stack.peek() == 'A') {
+                stack.pop();
+            }
+            else if (c == 'D' && stack.peek() == 'C') {
+                stack.pop();
+            } 
+            else {
+                stack.push(c);
+            }
+        }
+
+        return stack.size();
+    }
+}
