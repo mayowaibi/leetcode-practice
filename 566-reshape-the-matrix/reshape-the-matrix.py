0a96e7686540b1c973368b4873f6d17a2db363a7
Isaac <isaac.ibidun@gmail.com>
2024-10-20 22:31:26 -0400

    Time: 4 ms (96.77%) | Memory: 17.4 MB (24.42%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.py
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -1,28 +1,28 @@
 class Solution:
+    # TC: O(n*m), SC: O(n*m)
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         ROWS, COLS = len(mat), len(mat[0])
+
         # if reshape is not valid, return the original matrix
         if r * c != ROWS * COLS:
             return mat
         
-        # if r * c is valid we simply build the new matrix
-        new_mat = []
+        # if r * c is valid, build the new matrix
+        output = [[0 for x in range(c)] for x in range(r)]
 
-        # using a row to slowly element into the new matrix
-        row = []
+        # using a temp array to flatten original matrix
+        tmp = []
 
-        # we know the matrix is always 2d, we can use nested for loop
+        # add all nums in original matrix to temp array
         for i in range(ROWS):
             for j in range(COLS):
-                # here we are getting the numbers in order
-                # check if the row is the size of c (column)
-                if len(row) >= c:
-                    # reset the row
-                    new_mat.append(row)
-                    row = [mat[i][j]]
-                else:
-                    row.append(mat[i][j])
-        
-        # since new reshape r * c should fit exactly, we need to append row once more
-        new_mat.append(row)
-        return new_mat
+                tmp.append(mat[i][j])
+         
+        # add nums to output matrix from tmp
+        idx = 0     # to keep track of index in tmp
+        for i in range(r):
+            for j in range(c):
+                output[i][j] = tmp[idx]
+                idx += 1
+
+        return output
