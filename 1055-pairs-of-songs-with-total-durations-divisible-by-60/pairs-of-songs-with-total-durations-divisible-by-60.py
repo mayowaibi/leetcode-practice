9616e9885652f2f839f833bcaecdeaa5439f785e
Isaac <isaac.ibidun@gmail.com>
2024-11-01 16:52:18 -0400

    Time: 19 ms (79.21%) | Memory: 20.2 MB (88.29%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,18 +1,17 @@
 class Solution:
     # Hashmap + Mod Solution - TC: O(n), SC: O(1) [0, 59]
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        duration_map = defaultdict(int)     # to store counts of each duration % 60
+        duration_map = defaultdict(int)     # to store counts of each t % 60
         res = 0
 
         for t in time:
-            if t % 60 == 0:
-                # increment res by the count of previous songs with remainder 0
-                res += duration_map[0]
-            else:
-                # 60 - (t % 60) = number that can be added to t that would be a valid pair
-                res += duration_map[60-(t % 60)]
-            # add current t's remainder count to map
-            duration_map[t % 60] += 1
+            remainder = t % 60
+            # target is any number that can pair with current t
+            target = 0 if remainder == 0 else 60-remainder
+            # add target count to result
+            res += duration_map[target]
+            # update current t's remainder count to map
+            duration_map[remainder] += 1
 
         return res
 
