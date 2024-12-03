10d5c4e5394465e5883dd64486d54cbbaf04f0e6
Isaac <isaac.ibidun@gmail.com>
2024-12-02 23:50:47 -0500

    Time: 105 ms (51.90%) | Memory: 48.9 MB (40.48%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -1,18 +1,18 @@
 class Solution:
-    # Two Pointer Solution - TC: O(n) where n is the no. of chars in s, 
+    # Two Pointer Solution - TC: O(n+m) where n is the no. of chars in s, 
     # SC: O(n+m) where m is the len of spaces (no. of spaces to add)
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        res = ""
+        res = []    # using array then converting to string to save space (strings are immutable)
         sp_idx = 0
 
         for i in range(len(s)):
             # add space only if there are spaces to be added and
             # i matches the value of the current space's index
             if sp_idx < len(spaces) and i == spaces[sp_idx]:
-                res += " "
+                res.append(' ')
                 sp_idx += 1
 
             # add current char from s in every iteration
-            res += s[i]
+            res.append(s[i])
 
-        return res
+        return "".join(res)
