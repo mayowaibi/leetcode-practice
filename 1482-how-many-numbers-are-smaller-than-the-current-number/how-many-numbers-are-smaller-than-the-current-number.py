140e298778cec852f93db1522e59dbd2992f3d9c
Isaac <isaac.ibidun@gmail.com>
2024-12-07 01:11:38 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (5.50%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,6 +1,24 @@
 class Solution:
-    # Array Traversal Solution - TC: O(n^2), SC: O(n)
+    # Sorting + Hashmap Solution - TC: O(nlogn), SC: O(n)
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)
+        # key : value
+        # n   : index in sorted list (for first occurrence of num), also represents num of nums smaller than n
+        hashmap = {}
+
+        for i, num in enumerate(sorted_nums):
+            # only set num to index if it is the first occurrence
+            if num not in hashmap:
+                hashmap[num] = i
+
+        res = []
+        for num in nums:
+            res.append(hashmap[num])
+
+        return res
+
+    # Brute Force Solution - TC: O(n^2), SC: O(n) due to output array
+    def smallerNumbersThanCurrent2(self, nums: List[int]) -> List[int]:
         result = []
 
         for i in nums:
