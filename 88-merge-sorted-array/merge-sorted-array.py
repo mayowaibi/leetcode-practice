57a65e5fcdeb8128314af69b1e411ee68ef0bbe3
Isaac <isaac.ibidun@gmail.com>
2024-12-12 14:27:15 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (10.12%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -8,6 +8,7 @@ class Solution:
         j = n - 1   # point at last element in nums2
         k = m + n - 1   # point at last element in nums1
 
+        # merge in reverse order
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -17,6 +18,7 @@ class Solution:
                 j -= 1
             k -= 1
         
+        # fill nums1 with leftover nums2 elements, if any
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
