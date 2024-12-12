50162a9d1d3d4eeed5b00a85cfcfd9de81a64da0
Isaac <isaac.ibidun@gmail.com>
2024-12-12 14:25:09 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.24%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    # Three Pointer Solution - TC: O(n), SC: O(1)
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1   # point at last non-zero element in nums1
+        j = n - 1   # point at last element in nums2
+        k = m + n - 1   # point at last element in nums1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
