232184390eb62c07c672e9fcec1547711f14c23a
Isaac <isaac.ibidun@gmail.com>
2024-12-17 20:39:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.77%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -32,3 +32,25 @@ class Solution:
             left += 1
 
         return output
+
+    # Concise Popping Solution - TC: O(n*m), SC: O(1) if output matrix is excluded
+    def spiralOrder2(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        
+        while matrix:
+            # 1 - add all elements in first row
+            # use += instead of append to add elements rather than entire list
+            res += matrix.pop(0)
+            # 2 - add all the last elements in each row sequentially
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            # 3 - add all elements in bottom row in reverse order
+            if matrix:
+                res += matrix.pop()[::-1]
+            # 4 - add all the first elements in each row in reverse order 
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop(0))
+
+        return res
