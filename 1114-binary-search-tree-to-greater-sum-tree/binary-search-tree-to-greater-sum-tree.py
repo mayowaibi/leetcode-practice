d63f4ac18f3e248a3123343016760bd455236830
Isaac <isaac.ibidun@gmail.com>
2025-02-23 23:29:46 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.49%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Reverse Inorder Recursive DFS Traversal - TC: O(n), SC:O(n)
+    def bstToGst(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        curr_sum = 0
+
+        def dfs(node):
+            nonlocal curr_sum
+
+            if not node:
+                return
+            
+            dfs(node.right)
+            original = node.val
+            node.val += curr_sum
+            curr_sum += original
+            dfs(node.left)
+        
+        dfs(root)
+        return root
