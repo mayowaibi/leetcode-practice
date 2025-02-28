9b1cde7157b43f0e82f37bb33c3623a0dcf3d75e
Isaac <isaac.ibidun@gmail.com>
2025-02-28 04:33:59 -0500

    Time: 0 ms (100.00%) | Memory: 50 MB (68.04%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -5,24 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # BFS Solution - TC: O(n), SC: O(n)
+    # Iterative BFS Solution - TC: O(n), SC: O(n)
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        # Initialize the queue with the root node and its depth
-        queue = [(root, 1)]
-        
-        while queue:
-            # Dequeue the first node and its depth
-            node, depth = queue.pop(0)
-            
-            # If the node is a leaf, return its depth
+
+        q = collections.deque([(root, 1)])
+
+        while q:
+            node, depth = q.popleft()
+
             if not node.left and not node.right:
                 return depth
             
-            # Enqueue the left and right children if they exist
             if node.left:
-                queue.append((node.left, depth + 1))
+                q.append((node.left, depth + 1))
             if node.right:
-                queue.append((node.right, depth + 1))
+                q.append((node.right, depth + 1))
