b64a7c65886329c6770b42a4a8a5d69e3450abef
Isaac <isaac.ibidun@gmail.com>
2025-03-11 01:42:41 -0400

    Time: 16 ms (69.56%) | Memory: 17.9 MB (53.12%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,33 @@
 class Solution:
-    # Sliding Window: O(n) TC, O(n) SC
+    # Sliding Window Solution - TC: O(n), SC: O(n)
     def lengthOfLongestSubstring(self, s: str) -> int:
-        result = 0
+        res = 0
         charset = set()
+        
         l = 0
-
         for r in range(len(s)):
             # if there are duplicates in the range, remove the leftmost character and update the left pointer
             while s[r] in charset:
                 charset.remove(s[l])
                 l += 1
-            # if there are no duplicates in the range, add the current character (right pointer) to set
+            # if there are no duplicates in the range, add the current character to set
             charset.add(s[r])
-            # result is the length of the sliding window
-            windowLength = r - l + 1
-            result = max(result, windowLength)
 
-        return result
+            # update res to max window size seen
+            res = max(res, r - l + 1)
+
+        return res
+
+    # Brute Force Solution - TC: O(n^2), SC: O(n)
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        res = 0
+
+        for i in range(len(s)):
+            charSet = set()
+            for j in range(i, len(s)):
+                if s[j] in charSet:
+                    break
+                charSet.add(s[j])
+                res = max(res, j - i + 1)
+        
+        return res
