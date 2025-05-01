0a7de3b93b282696c245bb1595ab9b01138f6668
Isaac <isaac.ibidun@gmail.com>
2025-05-01 01:40:05 -0400

    Time: 83 ms (89.78%) | Memory: 27.5 MB (67.30%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,12 +1,16 @@
 class Solution:
+    # Two-Pointer Monotonic Stack Solution - TC: O(n),  SC: O(n)
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         result = [0] * len(temperatures)    # output array initialized with 0s
-        stack = []
+        stack = []  # storing indices of temperatures
 
         for r in range(len(temperatures)):
+
+            # while the current temp is higher than the last stored index's temp from stack
             while stack and temperatures[stack[-1]] < temperatures[r]:
-                l = stack.pop()
-                result[l] = r-l
-            stack.append(r)
+                l = stack.pop()  # get the index with the lower temp
+                result[l] = r-l  # calculate number of days until warmer temp
+
+            stack.append(r)  # add current index to the stack
         
         return result
