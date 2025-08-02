70e6a7c96524a8aa77313828458a88d2eeba548d
Isaac <isaac.ibidun@gmail.com>
2025-08-01 23:59:52 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.24%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,18 +1,27 @@
 class Solution:
-    # Recursive Solution - TC: O(n^3)
+    # "Dynamic Programming Solution" - TC: O(n^2), SC: O(n^2)
     def generate(self, numRows: int) -> List[List[int]]:
-        res = []
-        for n in range(1, numRows+1):
-            res.append(self.pascal(n))
+        res = [[1]]
+        for i in range(2, numRows+1):
+            res.append([1] + [res[-1][j] + res[-1][j+1] for j in range(i-2)] + [1])
         return res
-    
-    def pascal(self, n):
-        res = [0] * n
-        res[0] = res[-1] = 1
-        if n == 1 or n == 2:
-            return res
 
-        prev_list = self.pascal(n-1)
-        for i in range(1, n-1):
-            res[i] = prev_list[i-1] + prev_list[i]
+    # Inefficient Recursive Solution - TC: O(n^3), SC: n^2
+    def generate2(self, numRows: int) -> List[List[int]]:
+        res = []
+    
+        def pascal(n):
+            local_res = [0] * n
+            local_res[0] = local_res[-1] = 1
+            if n == 1 or n == 2:
+                return local_res
+
+            prev_list = pascal(n-1)
+            for i in range(1, n-1):
+                local_res[i] = prev_list[i-1] + prev_list[i]
+            return local_res
+
+        for n in range(1, numRows+1):
+            res.append(pascal(n))
+
         return res
