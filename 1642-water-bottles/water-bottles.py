5264767dc76a7a9dffa38f84fd5fdc029b701128
Isaac <isaac.ibidun@gmail.com>
2025-10-07 17:39:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.43%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,11 +1,17 @@
 class Solution:
+    # TC: O(logn) base e, SC: O(1)
+    # where n = numBottles and e = numExchange
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        res, empty = numBottles, numBottles
+        res = empty = 0
 
-        while empty >= numExchange:
-            newBottles = empty // numExchange
-            res += newBottles
-            empty = empty % numExchange
-            empty += newBottles
-        
+        while numBottles > 0:
+            # drink bottles
+            res += numBottles
+            empty += numBottles
+            numBottles = 0  # redundant but left for the sake of understanding
+
+            # exchange empty bottles
+            numBottles = empty // numExchange
+            empty %= numExchange
+
         return res
