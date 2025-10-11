9faaefdbee1adf9e138e90a4af160a66c8402304
Isaac <isaac.ibidun@gmail.com>
2025-10-11 01:03:00 -0400

    Time: 56 ms (16.94%) | Memory: 19.3 MB (43.43%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    # BFS Traversal - TC: O(n), SC: O(n)
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        res = []
+
+        if not root:
+            return res
+        
+        q = deque([root])
+
+        while q:
+            level = []
+
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                
+                for child in node.children:
+                    q.append(child)
+                
+            res.append(level)
+
+        return res
