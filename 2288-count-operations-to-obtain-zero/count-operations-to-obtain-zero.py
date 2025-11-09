ad2360311945b685fd935dfd329e2af7479da04e
Isaac <isaac.ibidun@gmail.com>
2025-11-08 22:35:27 -0500

    Time: 38 ms (68.97%) | Memory: 17.5 MB (96.78%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    # TC: O(max(num1, num2)), SC: O(1)
+    def countOperations(self, num1: int, num2: int) -> int:
+        res = 0
+
+        while num1 and num2:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            res += 1
+
+        return res
