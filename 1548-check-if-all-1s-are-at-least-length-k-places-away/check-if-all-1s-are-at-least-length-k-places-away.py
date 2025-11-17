aa902f1bc159f66bb3da7dcd0bec061c364a98f9
Isaac <isaac.ibidun@gmail.com>
2025-11-17 00:36:20 -0500

    Time: 12 ms (43.08%) | Memory: 21.1 MB (65.80%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.py
@@ -1,25 +1,19 @@
 class Solution:
+    # TC: O(n), SC: O(1)
     def kLengthApart(self, nums: List[int], k: int) -> bool:
         # var to keep track of distance between ones
-        dist = 0
-        # var to denote the first one
-        one_seen = False
+        # initialize to k to reset after first 1 is seen
+        dist = k
 
         for num in nums:
-            if one_seen:
-                # if 1 is encountered and dist >= k, reset dist to 0
-                if num == 1 and dist >= k:
-                    dist = 0
-                # if 1 is encountered and dist < k, return false
-                elif num == 1 and dist < k:
-                    return False
-                # if 0 is encountered, dist += 1
-                else:
-                    dist += 1
-
-            # mark when the first one has been found
+            # if 1 is encountered and dist < k, return False
             if num == 1:
-                one_seen = True
-        
-        # return true after going through all elements in list
+                if dist < k:
+                    return False
+                # if not, reset dist to 0
+                dist = 0
+            # if 0 is encountered, dist += 1
+            else:
+                dist += 1
+
         return True
