64e42123e978e3139ac49d03be4a3ec15e57433a
Isaac <isaac.ibidun@gmail.com>
2025-11-18 18:34:42 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.21%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Pre order traversal: Root, Left, Right
+class Solution:
+    # TC: O(n), SC: O(n)
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # 1. traverse through entire tree using a dfs pre-order traversal
+        # 2. store each node in a list
+        # [1, 2, 3, 4, 5, 6]
+        # 3. create a new tree with root as first node in list, 
+        # root = [1]
+        # 4. makes each subseq. element in list a right child of each node in the new tree
+        elements = []
+        def dfs(node):
+            if not node:
+                return
+            
+            elements.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        if len(elements) == 0:
+            return
+        # [1, 2, 3, 4, 5, 6]
+        # [1, null, 2, null, 3, null, 4, null, 5]
+
+        curr = root
+        curr.left = None
+        for v in elements[1:]:
+            curr.right = TreeNode(v, None, None)
+            curr = curr.right
